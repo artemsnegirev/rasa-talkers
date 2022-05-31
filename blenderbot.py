fe4bd395b8f5b545fc0a954fe143dff8b4ec8a4d
Artem Snegirev <a.snegirev@promo-bot.ru>
2022-05-31 13:23:25 +0500

    change default blenderbot-model

--- blenderbot.py
+++ blenderbot.py
@@ -1,26 +1,54 @@
-from typing import List, Text, Tuple
+from typing import List, Text
 
 from transformers import (
-    BlenderbotSmallTokenizer as Tokenizer, 
-    BlenderbotSmallForConditionalGeneration as Blenderbot
+    BlenderbotTokenizer as Tokenizer, 
+    BlenderbotForConditionalGeneration as Blenderbot
 )
+
+BLENDERBOT_CONTEXT_LENGTH = 128
 
 
 class Talker:
     def __init__(self, device='cpu') -> None:
         self.device = device
-
         self.__setup_model()
     
     def __setup_model(self):
-        name = "facebook/blenderbot_small-90M"
+        name = "facebook/blenderbot-400M-distill"
 
         self.model: Blenderbot = Blenderbot.from_pretrained(name)
         self.tokenizer: Tokenizer = Tokenizer.from_pretrained(name)
 
         self.model.to(self.device)
 
-    def __call__(self, context: List[Text]) -> Tuple[Text, List[Text]]:
+    def _truncate_to_max_length(self, inputs):
+        tokens = inputs['input_ids'][0]
+
+        if len(tokens) <= BLENDERBOT_CONTEXT_LENGTH:
+            return inputs
+
+        idx = BLENDERBOT_CONTEXT_LENGTH
+        truncation_idx = None
+        last_was_separator = False
+        
+        while idx > 0 and truncation_idx is None: 
+            if tokens[-idx] == 228:
+                last_was_separator = True
+            
+            elif last_was_separator:
+                truncation_idx = idx
+
+            idx = idx - 1
+
+        if truncation_idx is None:
+            truncation_idx = BLENDERBOT_CONTEXT_LENGTH
+
+        inputs['input_ids'] = inputs['input_ids'][..., -truncation_idx:]
+        inputs['attention_mask'] = inputs['attention_mask'][..., -truncation_idx:]
+
+        return inputs 
+
+    def __call__(self, context: List[Text]) -> Text:
         context = ['    '.join(context)]
 
         inputs = self.tokenizer(
@@ -28,11 +56,20 @@ class Talker:
             padding=True,
             truncation=True,
             return_tensors="pt"
-        )
+        ).to(self.device)
 
-        inputs.to(self.device)
+        inputs = self._truncate_to_max_length(inputs)
 
         output = self.model.generate(**inputs)
         output.to('cpu')
 
-        return self.tokenizer.batch_decode(output, skip_special_tokens=True)[0]
+        response = self.tokenizer.batch_decode(
+            output, 
+            skip_special_tokens=True
+        )[0]
+
+        return self.__remove_extra_spaces(response)
+    
+    @staticmethod
+    def __remove_extra_spaces(s: Text) -> Text:
+        return ' '.join(s.split())
