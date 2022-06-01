0b9a89479df61f1a90814d50bad1f790666599f6
Artem Snegirev <a.snegirev@promo-bot.ru>
2022-06-01 20:14:21 +0500

    add custom truncation function

--- blenderbot.py
+++ blenderbot.py
@@ -7,7 +7,36 @@ from transformers import (
     BlenderbotForConditionalGeneration as Blenderbot
 )
 
-BLENDERBOT_CONTEXT_LENGTH = 128
+
+def tokenize_conversation(self: Tokenizer, conversation: Conversation) -> List[int]:
+    full_string = "    ".join([
+        text for _, text in conversation.iter_texts()
+    ])
+
+    input_ids = self.encode(full_string)
+
+    if len(input_ids) <= self.model_max_length:
+        return input_ids
+
+    idx = self.model_max_length
+    truncation_idx = None
+    last_was_separator = False
+    
+    while idx > 0 and truncation_idx is None: 
+        if input_ids[-idx] == 228:
+            last_was_separator = True
+        
+        elif last_was_separator:
+            truncation_idx = idx
+
+        idx = idx - 1
+
+    if truncation_idx is None:
+        truncation_idx = self.model_max_length
+
+    input_ids = input_ids[-truncation_idx:]
+
+    return input_ids
 
 
 class Talker:
@@ -18,12 +47,18 @@ class Talker:
     def __setup_model(self):
         name = "facebook/blenderbot-400M-distill"
 
+        # https://github.com/huggingface/transformers/blob/v4.19.2/src/transformers/models/blenderbot/tokenization_blenderbot.py#L77
+        Tokenizer._build_conversation_input_ids = tokenize_conversation
+
         self.model: Blenderbot = Blenderbot.from_pretrained(name)
         self.tokenizer: Tokenizer = Tokenizer.from_pretrained(name)
 
         self.pipeline = pipeline(
+            model=self.model,
+            tokenizer=self.tokenizer,
             task="conversational", 
             device=self.device,
+            framework='pt'
         )
 
     def __call__(self, dialog: List[Text]) -> Text:
@@ -41,7 +76,7 @@ class Talker:
         conversation: Conversation = self.pipeline(
             conversation,
 
-            max_length=BLENDERBOT_CONTEXT_LENGTH,
+            max_length=self.tokenizer.model_max_length,
             pad_token_id=self.tokenizer.eos_token_id,
         )
 
